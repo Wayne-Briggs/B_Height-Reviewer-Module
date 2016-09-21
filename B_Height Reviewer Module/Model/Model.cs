2c492bbadcf6cb84a5270a4186f160d9018c9df9
Wayne-Briggs <wayne.briggs@savvytechnologies.co.uk>
2016-09-21 09:41:04 +0100

    First commit, empty template.

---
+++ B_Height Reviewer Module/Model/Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_Height_Reviewer_Module.Model
+{
+    public class Model
+    {
+    }
+}
